05f97e4de85948e6bdc94057347c275a23ab79c7
Abhishek Kumar <abhi24033c@gmail.com>
2025-11-03 11:08:17 +0530

    cors changed

--- index.js
+++ index.js
@@ -14,7 +14,19 @@ connectDB();
 const app = express();
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+// app.use(cors());
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:5173/",
+  "https://familytree-frontend.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => res.send("MERN Family Tree Backend"));
